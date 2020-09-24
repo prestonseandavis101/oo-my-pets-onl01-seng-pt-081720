71b47a10b5cae4977bd360936a9e46f71df890f0
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-24 00:22:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,47 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@owner = []
+  
+  def self.owner
+    @@owner
+  end
+  
+  def initialize(name)
+    @name=name
+    @species= "human"
+    self.class.owner << self
+    
+  end
+  
+  
+  def say_species
+    return "I am a #{@species}."
+  end
+  
+  def self.all 
+    self.owner
+  end
+  
+  def self.count
+    self.owner.length 
+  end
+  
+  def self.reset_all
+    @@owner = []
+  end
+  
+  def cats
+    Cat.all.select do |cat|
+     cat.owner == self
+    #binding.pry
+    end
+  end
+  
+  def dogs 
+    Dog.all.select do |dog|
+      dog.owner == self
+    end
+  end
 end
